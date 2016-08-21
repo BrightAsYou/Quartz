fd9156627b7508a5c5c4ea35bbd37b20f54d9554
renfuhao01 <renfuhao01@163.com>
2016-08-21 18:48:43 +0800

    首次基线

---
+++ Quartz/src/prs/rfh/schedule/spring/JobLogin.java
@@ -0,0 +1,19 @@
+package prs.rfh.schedule.spring;
+
+
+import org.springframework.stereotype.Service;
+
+@Service("login")
+public class JobLogin {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void release(){
+		System.out.println("releaseing a car");
+	}
+	
+	
+	
+}
